8307fefcd422e87d87d34156bf5835edf8df4c05
Bato Balint <balint.bato@gmail.com>
2021-10-06 15:15:17 +0200

    hozzatolt metodusig kesz

--- src/hu/petrik/Palack.java
+++ src/hu/petrik/Palack.java
@@ -32,6 +32,20 @@ public class Palack {
         return this.maxUrtartalom / 35 + this.jelenlegiUrtartalom;
     }
 
+    public boolean egyezik(Palack masik) {
+        boolean egyezik = false;
+        if (this.nev.equals(masik.nev)
+                && this.jelenlegiUrtartalom == masik.jelenlegiUrtartalom
+                && this.maxUrtartalom == masik.maxUrtartalom) egyezik = true;
+        return egyezik;
+    }
+
+    public void hozzaOnt(Palack hozzatoltendo) {
+        this.setJelenlegiUrtartalom(this.jelenlegiUrtartalom + hozzatoltendo.jelenlegiUrtartalom);
+        if (this.nev == null) this.nev = hozzatoltendo.nev;
+        else if (!this.nev.equals(hozzatoltendo.nev)) this.nev = "keverek";
+    }
+
     @Override
     public String toString() {
         return String.format("Palack, benne levo ital: $s, jelenleg: %d ml van benne, maximum %d ml fer bele.",
